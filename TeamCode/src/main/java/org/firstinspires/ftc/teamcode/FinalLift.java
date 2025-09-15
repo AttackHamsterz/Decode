e9b109f21aa22adec3f5b8c95a6d3e232c847883
amelialee38743 <amelialee38743@gmail.com>
2025-09-14 20:08:45 -0500

    Final lift starting class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FinalLift {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class FinalLift extends RobotPart<FinalLiftMetric>{
+    public FinalLift(StandardSetupOpMode ssom){
+        this.ssom = ssom;
+        this.gamepad = ssom.gamepad1;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    protected void setTo(FinalLiftMetric metric) {
+
+    }
+
+    @Override
+    public void safeHold() {
+
+    }
+
+    @Override
+    protected boolean closeEnough(FinalLiftMetric metric) {
+        return false;
+    }
+
+    @Override
+    public void getTelemetry(Telemetry telemetry) {
+
+    }
 }
+
+
