74e59c3a207312849bfcd29913de9095041048a3
amelialee38743 <amelialee38743@gmail.com>
2025-09-14 20:48:17 -0500

    code to lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
@@ -2,12 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 import android.net.EthernetNetworkSpecifier;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class FinalLift extends RobotPart<FinalLiftMetric>{
+
+    public final DcMotorEx leftLift, rightLift;
+
     public FinalLift(StandardSetupOpMode ssom){
         this.ssom = ssom;
         this.gamepad = ssom.gamepad1;
+        leftLift = ssom.hardwareMap.get(DcMotorEx.class, "leftLiftDrive"); //need to define channel
+        rightLift = ssom.hardwareMap.get(DcMotorEx.class, "rightLiftDrive"); //need to define channel
+
+        leftLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftLift.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightLift.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
