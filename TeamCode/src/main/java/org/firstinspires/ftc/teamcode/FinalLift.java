3b7a7143b294fd021a6e21bbc24cf9f7ea200ac9
Mark McDonnell <mcdonnellshouse@gmail.com>
2026-01-25 19:57:31 -0600

    lift height

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalLift.java
@@ -12,13 +12,13 @@ public class FinalLift extends RobotPart<FinalLiftMetric>{
     private final DcMotor finalLiftMotorRight;
     private static final double PPM = 1993.6;
     private static final double PPM_IN = 4.75;
-    private static final double MAX_LIFT_HEIGHT_LEFT_IN = 20.0;
-    private static final double MAX_LIFT_HEIGHT_RIGHT_IN = 20.5;
+    private static final double MAX_LIFT_HEIGHT_LEFT_IN = 21.0;
+    private static final double MAX_LIFT_HEIGHT_RIGHT_IN = 21.5;
     private static final double LIFT_HEIGHT_INCREMENT_LEFT_IN = MAX_LIFT_HEIGHT_LEFT_IN / 5.0;
     private static final double LIFT_HEIGHT_INCREMENT_RIGHT_IN = MAX_LIFT_HEIGHT_RIGHT_IN / 5.0;
     private static final double LIFT_HEIGHT_LEFT_START_IN = MAX_LIFT_HEIGHT_LEFT_IN * 0.9;
     private static final double LIFT_HEIGHT_RIGHT_START_IN = MAX_LIFT_HEIGHT_RIGHT_IN * 0.9;
-    private static final double LIFT_POWER_LEFT = 0.97;
+    private static final double LIFT_POWER_LEFT = 0.96;
     private static final double LIFT_POWER_RIGHT = 1.0;
     private static final double DRAG_HEIGHT_INCREMENT_LEFT_IN = 0.45;
     private static final double DRAG_HEIGHT_INCREMENT_RIGHT_IN = 0.45;
